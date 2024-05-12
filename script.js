d65d44d450df308a999a831567789e7989f6e7fa
RIBERA789 <32884666+RIBERA789@users.noreply.github.com>
2024-05-12 14:12:09 -0700

    cambie parametros pero no recuerdo donde

--- script.js
+++ script.js
@@ -9,52 +9,52 @@ let arriba = document.querySelector(".superior")
 let ancho = 10;
 let indiceActual = 0
 let indiceManzana = 0
-let serpienteActual = [2,1,0]
+let serpienteActual = [2, 1, 0]
 let direccion = 1
 let puntaje = 0
 let velocidad = 0.8
 let tiempoIntervalo = 0
 let interval = 0
 
-   document.addEventListener("DOMContentLoaded", function(){
-  	document.addEventListener("keyup", control)
-  	crearTablero()
-  	
-  	
-  	juegaDeNuevo.addEventListener("click", repeticion)
-  })
-  
-  function crearTablero(){
+document.addEventListener("DOMContentLoaded", function () {
+	document.addEventListener("keyup", control)
+	crearTablero()
 
-  	popup.style.display = "none";
-  	for (let i = 0;i < 100;i++) {
-  		 let div = document.createElement("div")
-         cuadrilla.appendChild(div);
-        
-  	}
+
+	juegaDeNuevo.addEventListener("click", repeticion)
+})
+
+function crearTablero() {
+
+	popup.style.display = "none";
+	for (let i = 0; i < 100; i++) {
+		let div = document.createElement("div")
+		cuadrilla.appendChild(div);
+
+	}
 }
 
-function comienzaJuego(){
+function comienzaJuego() {
 
 	let cuadrados = document.querySelectorAll(".cuadrilla div")
-     manzanaAlAzar(cuadrados)
-   
- 
+	manzanaAlAzar(cuadrados)
 
-    direccion = 1;
-    muestraPuntaje.innerHTML = puntaje
-    tiempoIntervalo = 1000
-    serpienteActual = [2,1,0]
-    indiceActual = 0
-    serpienteActual.forEach(index => cuadrados[index].classList.add("serpiente"))
-    interval = setInterval(moverResultado, tiempoIntervalo)
+
+
+	direccion = 1;
+	muestraPuntaje.innerHTML = puntaje
+	tiempoIntervalo = 1000
+	serpienteActual = [2, 1, 0]
+	indiceActual = 0
+	serpienteActual.forEach(index => cuadrados[index].classList.add("serpiente"))
+	interval = setInterval(moverResultado, tiempoIntervalo)
 
 }
 
-function moverResultado(){
+function moverResultado() {
 
 	let cuadrados = document.querySelectorAll(".cuadrilla div")
-	if(compruebaPorGolpes(cuadrados)){
+	if (compruebaPorGolpes(cuadrados)) {
 		alert("le pegastes a algo")
 		popup.style.display = "flex"
 		return clearInterval(interval)
@@ -72,64 +72,64 @@ function mueveSerpiente(cuadrados) {
 	cuadrados[serpienteActual[0]].classList.add("serpiente")
 
 }
-function compruebaPorGolpes(cuadrados){
- if(
-     (serpienteActual[0] + ancho >= (ancho * ancho) && direccion === ancho ) ||
-      (serpienteActual[0] % ancho === ancho -1 && direccion === 1) ||
-      (serpienteActual[0] % ancho === 0 && direccion === -1) ||
-      (serpienteActual[0] - ancho <= 0 && direccion === -ancho) ||
-        cuadrados[serpienteActual[0] + direccion].classList.contains("serpiente") 
-        ){
+function compruebaPorGolpes(cuadrados) {
+	if (
+		(serpienteActual[0] + ancho >= (ancho * ancho) && direccion === ancho) ||
+		(serpienteActual[0] % ancho === ancho - 1 && direccion === 1) ||
+		(serpienteActual[0] % ancho === 0 && direccion === -1) ||
+		(serpienteActual[0] - ancho <= 0 && direccion === -ancho) ||
+		cuadrados[serpienteActual[0] + direccion].classList.contains("serpiente")
+	) {
 		return true;
 	} else {
 		return false;
+	}
 }
-}
- function comeManzana(cuadrados, cola){
-   if(cuadrados[serpienteActual[0]].classList.contains("manzana")){
+function comeManzana(cuadrados, cola) {
+	if (cuadrados[serpienteActual[0]].classList.contains("manzana")) {
 
-     cuadrados[serpienteActual[0]].classList.remove("manzana");
-     cuadrados[cola].classList.add("serpiente")
-     serpienteActual.push(cola)
-     manzanaAlAzar(cuadrados)
-     puntaje++ 
-     muestraPuntaje.textContent = puntaje
-     clearInterval(interval)
-     tiempoIntervalo = tiempoIntervalo * velocidad
-     interval = setInterval(moverResultado, tiempoIntervalo)
+		cuadrados[serpienteActual[0]].classList.remove("manzana");
+		cuadrados[cola].classList.add("serpiente")
+		serpienteActual.push(cola)
+		manzanaAlAzar(cuadrados)
+		puntaje++
+		muestraPuntaje.textContent = puntaje
+		clearInterval(interval)
+		tiempoIntervalo = tiempoIntervalo * velocidad
+		interval = setInterval(moverResultado, tiempoIntervalo)
 
-   }
+	}
 }
 
 function manzanaAlAzar(cuadrados) {
 	do {
-   indiceManzana = Math.floor(Math.random() * cuadrados.length)
-} while(cuadrados[indiceManzana].classList.contains("serpiente"))
-    cuadrados[indiceManzana].classList.add("manzana")
+		indiceManzana = Math.floor(Math.random() * cuadrados.length)
+	} while (cuadrados[indiceManzana].classList.contains("serpiente"))
+	cuadrados[indiceManzana].classList.add("manzana")
 
 }
 function control(e) {
-	if (e.keycode === 39){
+	if (e.keycode === 39) {
 		direccion = 1; //derecha
-	 } else if (e.keycode === 38) {
-	 	direccion = -ancho // siempre presionamos la flecha de arriba
-	 } else if(e.keycode === 37){
-	 	direccion = -1 // izquierda la serpiente ira un div a la izquieda
-	 } else if (e.keycode === 40) {
-	 	direccion = +ancho; // la serpiente ira 10 div abajo
-	 }
-	
-}
-arriba.addEventListener("click",()=>direccion = -ancho)
-fondo.addEventListener("click",()=>direccion = +ancho)
-izquierda.addEventListener("click",()=> direccion = -1)
-derecha.addEventListener("click",()=>direccion = 1 )
+	} else if (e.keycode === 38) {
+		direccion = -ancho // siempre presionamos la flecha de arriba
+	} else if (e.keycode === 37) {
+		direccion = -1 // izquierda la serpiente ira un div a la izquieda
+	} else if (e.keycode === 40) {
+		direccion = +ancho; // la serpiente ira 10 div abajo
+	}
 
-function repeticion(){
+}
+arriba.addEventListener("click", () => direccion = -ancho)
+fondo.addEventListener("click", () => direccion = +ancho)
+izquierda.addEventListener("click", () => direccion = -1)
+derecha.addEventListener("click", () => direccion = 1)
+
+function repeticion() {
 
 	cuadrilla.innerHTML = " "
 	crearTablero()
 	comienzaJuego()
-    popup.style.display = "none";
+	popup.style.display = "none";
 
 }
